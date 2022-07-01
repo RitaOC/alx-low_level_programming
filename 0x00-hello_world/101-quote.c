3521a8dd0718d097bb584b6b4fede7f876add024
RitaOC <onwudiwechinwendu16@gmail.com>
2022-06-30 17:22:13 -0700

    Advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char stri[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, stri, 59);
+return (1);
+}
